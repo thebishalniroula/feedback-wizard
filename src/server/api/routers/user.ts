de5552bd5f0bc4b6d9f865483450f2a2f3f51392
Bishal Niroula <niroulabishal199@gmail.com>
2023-05-14 00:26:54 +0545

    fixed deployment

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -3,51 +3,51 @@ import { z } from 'zod'
 import { createTRPCRouter, publicProcedure } from '~/server/api/trpc'
 
 export const userRouter = createTRPCRouter({
-  create: publicProcedure
-    .input(
-      z.object({
-        name: z.string(),
-        email: z.string().email(),
-        username: z.string(),
-        password: z.string(),
-      })
-    )
-    .mutation(({ ctx, input }) => {
-      const { name, email, password, username } = input
-      return ctx.prisma.user.create({
-        data: {
-          name,
-          email,
-          password,
-          username,
-        },
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          username: true,
-        },
-      })
-    }),
-  verifyCredentials: publicProcedure
-    .input(z.object({ email: z.string(), password: z.string() }))
-    .query(async ({ ctx, input }) => {
-      const user = await ctx.prisma.user.findFirst({
-        where: {
-          email: input.email,
-        },
-      })
-      if (!user) {
-        return null
-      }
-      if (user?.password === input.password) {
-        return null
-      }
-      return {
-        id: user.id,
-        name: user.name,
-        username: user.username,
-        email: user.email,
-      }
-    }),
+  // create: publicProcedure
+  //   .input(
+  //     z.object({
+  //       name: z.string(),
+  //       email: z.string().email(),
+  //       username: z.string(),
+  //       password: z.string(),
+  //     })
+  //   )
+  //   .mutation(({ ctx, input }) => {
+  //     const { name, email, password, username } = input
+  //     return ctx.prisma.user.create({
+  //       data: {
+  //         name,
+  //         email,
+  //         password,
+  //         username,
+  //       },
+  //       select: {
+  //         id: true,
+  //         name: true,
+  //         email: true,
+  //         username: true,
+  //       },
+  //     })
+  //   }),
+  // verifyCredentials: publicProcedure
+  //   .input(z.object({ email: z.string(), password: z.string() }))
+  //   .query(async ({ ctx, input }) => {
+  //     const user = await ctx.prisma.user.findFirst({
+  //       where: {
+  //         email: input.email,
+  //       },
+  //     })
+  //     if (!user) {
+  //       return null
+  //     }
+  //     if (user?.password === input.password) {
+  //       return null
+  //     }
+  //     return {
+  //       id: user.id,
+  //       name: user.name,
+  //       username: user.username,
+  //       email: user.email,
+  //     }
+  //   }),
 })

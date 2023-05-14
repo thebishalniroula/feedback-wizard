f54dfba52cdcf08500971ff825090e427156df19
Bishal Niroula <niroulabishal199@gmail.com>
2023-05-14 14:49:25 +0545

    feat: copy sharable link to clipboard

--- src/pages/dashboard/form/edit/[slug].tsx
+++ src/pages/dashboard/form/edit/[slug].tsx
@@ -6,6 +6,7 @@ import { NextPageWithLayout } from '~/pages/_app'
 import { api } from '~/utils/api'
 import Loading from '~/components/ui/Loading'
 import { Question, type Form, Submission } from '~/server/db'
+import Link from 'next/link'
 
 type QuestionsStateType = {
   [questionId: string]: string
@@ -20,6 +21,7 @@ const convertObjectToArray = (obj: QuestionsStateType | undefined) => {
 
 const Form: NextPageWithLayout = () => {
   const router = useRouter()
+
   const { slug } = router.query
   if (!slug) return null
   if (Array.isArray(slug)) return null
@@ -48,7 +50,7 @@ const Form: NextPageWithLayout = () => {
     },
   ]
   return (
-    <>
+    <div className=''>
       <div className='relative w-full h-52'>
         <Image
           src={
@@ -60,10 +62,41 @@ const Form: NextPageWithLayout = () => {
           alt={form.title}
         />
       </div>
+
       <h1 className='text-center text-3xl mt-4 mb-3'>{form.title}</h1>
-      <p className='text-center text-slate-200 mb-4'>{form.description}</p>
+      <p className='text-center text-slate-200 mb-6'>{form.description}</p>
+      <div className='flex flex-col justify-center items-center mb-5'>
+        {window !== undefined && (
+          <Link className='text-slate-400 underline' href={`${window.location.origin}/form/${form.slug}`}>
+            {`${window.location.origin}/form/${form.slug}`}
+          </Link>
+        )}
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText(`${window.location.origin}/form/${form.slug}`)
+            window && window.alert('Link copied to clipboard.')
+          }}
+          className='flex gap-3 my-5 py-2 px-4 bg-blue-800 text-slate-200 font-semibold shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            className='w-6 h-6'
+          >
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z'
+            />
+          </svg>
+          Copy
+        </button>
+      </div>
       <Tabs tabs={tabsData} />
-    </>
+    </div>
   )
 }
 Form.hasLayout = true
